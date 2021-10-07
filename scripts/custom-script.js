83076153e13b854abcf8958dbd30a65555ab2962
Anam Farooque <92043152+AnamFarooque@users.noreply.github.com>
2021-10-07 14:50:06 +0500

    Add files via upload

---
+++ scripts/custom-script.js
@@ -0,0 +1,34 @@
+/*================================================================================
+Item Name: DexterLabs - DexterLabs Design Admin Template
+Version: 3.1
+Author: DexterLabs
+Author URL: http://themeforest.net/user/qasimdesigner
+================================================================================
+
+NOTE:
+------
+PLACE HERE YOUR OWN JS CODES AND IF NEEDED.
+WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR CUSTOM SCRIPT IT'S BETTER LIKE THIS. */
+jQuery(function () {
+
+//    jQuery('.lnkConfigStore').click(function () {
+//        
+//        jQuery("#modal").iziModal({
+//            iframe: true,
+//            history: false,
+//            iframeHeight: 600,
+//            iframeURL: "/StoreConfig.aspx?companyID=" + parent._companyID,
+//            title: 'Stores Configuration',
+//            headerColor: '#000',
+//            width: 800,
+//            borderBottom: false,
+//            closeOnEscape: true
+
+//        });
+
+//        jQuery('#modal').iziModal('open');
+//        return false;
+
+//    });
+
+});
